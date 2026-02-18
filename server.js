668da7af7897e228de55c4ddaa05e74a353cadec
yashparmar-growexxer <yash.parmar@growexx.com>
2026-02-18 10:57:29 +0530

    fix railway port

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
